33dc8bd48b9b32057548a31f74291200bbdb53da
TheWangYang <wyysuccess2020@163.com>
2023-09-11 16:02:45 +0800

    Update this repo by adding detailed notes.

--- version_1/README.md
+++ version_1/README.md
@@ -2,10 +2,19 @@
 
 参考：[简单web服务器的实现（C++）](https://blog.csdn.net/qq_36573828/article/details/82784425)
 
-### 包含功能
+### version_1
+
+**包含功能**
 
 1. 处理get和post请求；
 2. 返回html界面；
 3. 将linux中tcp socket实现方式在windows进行了适配；
+
+### version_2
+
+**包含功能**
+
+
+
 
 
--- version_1/simple_webserver.cpp
+++ version_1/simple_webserver.cpp
@@ -12,6 +12,7 @@ int main() {
         return 1;
     }
 
+    //定义socket套接字对象
     SOCKET sock;
     SOCKET connfd;
     struct sockaddr_in sever_address;
@@ -52,51 +53,68 @@ int main() {
     while (1) {
         struct sockaddr_in client;
         int client_addrlength = sizeof(client);
+        //接收客户端的连接请求，并创建套接字对象
         connfd = accept(sock, (struct sockaddr*)&client, &client_addrlength);
         printf("here 1");
 
+        //表示接收到错误的套接字请求
         if (connfd == INVALID_SOCKET) {
             fprintf(stderr, "Accept failed.\n");
         } else {
+            //接收到正确的套接字请求
             char request[1024];
-            int bytesReceived = recv(connfd, request, sizeof(request) - 1, 0);
 
+            //得到recv函数返回字节数结果
+            int bytesReceived = recv(connfd, request, sizeof(request) - 1, 0);
+            
+            //如果字节数为-1表示接收错误
             if (bytesReceived == -1) {
                 // 处理接收错误
                 printf("接收错误");
             } else {
+                //接收成功
                 request[bytesReceived] = '\0'; // 手动添加 null 终止字符
                 printf("%s\n", request);
                 printf("Request Successfully!\n");
 
+                //HTTP响应头
                 char buf[520] = "HTTP/1.1 200 OK\r\nConnection: close\r\n\r\n"; // HTTP响应
                 int s = send(connfd, buf, strlen(buf), 0); // 发送响应
 
+                // 发送响应头之后，继续发送响应数据内容
                 // 调用.html界面
                 FILE *file = fopen("E:\\_my_github_repositories\\my_cpp_server\\version_1.0\\html\\hello.html", "rb");
                 if (file) {
-                    fseek(file, 0, SEEK_END);
-                    long fileSize = ftell(file);
-                    fseek(file, 0, SEEK_SET);
-
+                    fseek(file, 0, SEEK_END);//将文件指针移动到最后
+                    long fileSize = ftell(file);//得到文件指针对应的开始位置的偏移量（就是文件长度）
+                    fseek(file, 0, SEEK_SET);//将文件指针重新放回到文件的开始位置
+                    //给文件分配内存地址
                     char *fileBuffer = (char *)malloc(fileSize);
+
+                    //如果分配地址成功
                     if (fileBuffer) {
+                        //读取文件内容
                         fread(fileBuffer, 1, fileSize, file);
+                        //发送到客户端界面进行HTML页面的显示，其中参数0表示默认行为
                         send(connfd, fileBuffer, fileSize, 0);
+                        // 释放文件buffer缓存地址
                         free(fileBuffer);
                     }
-
+                    // 关闭文件指针
                     fclose(file);
                 }
             }
 
+            // 关闭套接字对象
             closesocket(connfd);
         }
     }
 
     printf("here 3");
+    //关闭服务器端套接字对象
     closesocket(sock);
+    //清理windows套接字编程需要的资源，为了避免不必要的资源泄露情况出现
     WSACleanup();
-
+    
     return 0;
 }
